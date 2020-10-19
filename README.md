fd16ffab5f4018bbfe0d1361f78d8f48bc6850d8
srishanthneela <72262599+srishanthneela@users.noreply.github.com>
2020-10-19 18:59:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# 25--project
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ dusbin.js
@@ -0,0 +1,19 @@
+class Dustbin {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("sprites/dustbingreen.png");
+    
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill("white");
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  }
